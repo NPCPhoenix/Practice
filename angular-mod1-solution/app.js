f2b53234ac30fb2c289b4ac939bb4458a1a25fd3
Beifei <beifei.wang@gmail.com>
2017-01-23 00:09:05 +0800

    angular mod solution 1

---
+++ angular-mod1-solution/app.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController ($scope) {
+  $scope.dishes = '';
+  $scope.message = '';
+
+  $scope.check = function () {
+  	var items = $scope.dishes.split(',');
+  	if (items.length == 0) {
+  		$scope.message = 'Please enter data first'
+  	}
+  	else if (items.length <4) {
+  		$scope.message = 'Enjoy'
+  	}
+  	else {
+  		$scope.message = 'Too Much'
+  	}
+  };
+}
+
+})();
+
